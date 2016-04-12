488a12933319ea641eaefc08ccecac62b552c202
Hongchen Li <apple1003232@gmail.com>
2016-04-12 14:56:39 -0700

    add multithreading

---
+++ GoogleCrawler_MongoDB_multithreading/README.txt
@@ -0,0 +1,48 @@
+Google Crawler (Python 2.7)
+
+This is a basic tool to crawls google for user-inputted search terms with multithreading. It saves the first 20 results returned, visits each of the result pages and stores the title, main text and meta description in Mongodb database. 
+
+Author: Hongchen
+===============================================================
+METHODS:
+
+1. This tool crawls google with the Google API. This free API allows me to return up to 4 results in a single call. To get the first 20 results, I called 4 times. 20 results are stored in a queue.
+
+2. Create 13 threads. For each thread, get one result from the queue, and apply python-goose to extract the title, main text and meta description of each result page. Goose can also be useful in video and image extraction. In this project, I only extracted the main text from the html page.
+
+3. Store the information extracted in Mongodb database.
+
+Note: Since only 20 results are required to return, 
+
+===============================================================
+SOFTWARE TOOLS:
+
+(1) Python 2.7.10
+(2) python-goose (for installation details:  https://github.com/grangier/python-goose)
+(3) Mongodb (for installation details: https://docs.mongodb.org/manual/tutorial/install-mongodb-on-os-x/)
+(4) Robomongo (optional)
+
+===============================================================
+SOURCE CODE FILES:
+
+(1) google.py
+Given the inputted search terms, crawl google, return the first 20 results and store the useful information of each result page to Mongodb in multithreadings.
+
+===============================================================
+Here is an example usage of Google Crawler:
+
+(1) Enter key words "python", and crawl google :
+$ python google.py python
+
+The useful information for the first 20 results will be stored in the MongoDB. You can used Robomongo to view the data in the database if you like.
+
+ =============================================================
+ TODO:
+
+ Video and image extraciton
+
+
+
+
+
+ 
---
+++ GoogleCrawler_MongoDB_multithreading/google_multithreading.py
@@ -0,0 +1,118 @@
+import urllib
+import json
+import sys
+import Queue
+import threading
+import time
+
+from goose import Goose
+from pymongo import MongoClient
+
+exitFlag = 0
+
+
+class  myThread(threading.Thread):
+	def __init__(self, threadID, name, q):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.q = q
+
+	def run(self):
+		print "Starting " + self.name
+		process_data(self.name, self.q)
+		print "Exiting " + self.name
+
+def process_data(threadName, q):
+	while not exitFlag:
+		queueLock.acquire()
+		if not workQueue.empty():
+			global Id
+			print "%s processing No.%s result page..." % (threadName, Id)
+			data = q.get()
+			g = Goose()
+			resultUrl = data["unescapedUrl"]
+			article = g.extract(url = resultUrl)
+			item = {}
+			item['title'] = data["titleNoFormatting"]
+			item['url'] = resultUrl
+			item['keyWords'] = keyWords
+			item['description'] = article.cleaned_text[:4000]
+			insert(item)
+			Id += 1
+			
+			queueLock.release()
+			
+			
+		else:
+			queueLock.release()
+		time.sleep(1)
+
+
+def insert(item):
+	# connect to MongoDB 
+	client = MongoClient('localhost', 27017)
+	db = client['google']
+	collection = db['results']
+	if collection.find({'url': item['url']}).count() == 0:
+		collection.insert_one(item)
+
+def GoogleSearch(argu):
+	url2 = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&start="
+
+	q = "&q="
+	keyWords = argu #"python"
+	startNums = ["0","4","8","12","16"]
+
+	
+	print "Start to crawl Google with keyWords: %s" % keyWords
+
+	# store 20 results in a Queue
+	for num in startNums:
+		theUrl = url2 + num + q + keyWords
+		f = urllib.urlopen(theUrl)
+		j = json.load(f)
+		for res in j["responseData"]["results"]:
+			workQueue.put(res)
+
+	
+##########################################################
+start_time = time.clock()
+keyWords = ""
+for ele in sys.argv[1:]:
+	keyWords += " " + ele
+
+
+threadList = []
+for i in range(13):
+	threadList.append("Thread-" + str(i+1))
+
+queueLock = threading.Lock()
+workQueue = Queue.Queue(20)
+threads = []
+threadID = 1
+Id = 1
+
+for tName in threadList:
+	thread = myThread(threadID, tName, workQueue)
+	thread.start()
+	threads.append(thread)
+	threadID += 1
+
+queueLock.acquire()
+GoogleSearch(keyWords)
+queueLock.release()
+
+
+while not workQueue.empty():
+	pass
+
+exitFlag = 1
+
+for t in threads:
+	t.join()
+print "Exiting Main Thread"
+print "-----End-----"
+end_time = time.clock()
+print end_time - start_time
+
